98d096714a6633453b5a799dc7c15c5c4e77b5d3
Nonlineary <nonlineary@naver.com>
2021-05-16 01:03:37 +0900

    Introduction to R

---
+++ Statistics-and-Data-Science/1-1-Statistical-Computing-with-R/ex01-06.R
@@ -0,0 +1,9 @@
+x <- c(7,2,4,9,8,6,1,5,10,3)
+sort(x) #숫자를 오름차순으로 정렬
+sum(x)
+mean(x) #평균
+sum(x)/length(x) #length() 원소의 갯수 : 합을 갯수로 나누었으몰 mean(x)와 동일한 값이 나옴
+var(x) #분산
+min(x)
+which.min(x) #최솟값이 있는 위치 : 1부터 카운팅
+summary(x) #기초 통계량 : 평균, 중간값, 최대, 최소, 사분위수(quartile)
---
+++ Statistics-and-Data-Science/1-1-Statistical-Computing-with-R/ex01-07.R
@@ -0,0 +1,6 @@
+y <- c(1,3,5,NA,9,NA,11) #결측치가 있을 때 함수 실행결과도 NA
+mean(y)
+mean(y,na.rm=TRUE) # 또는 na.rm=T 결측치를 무시하고 계산하고 싶을 때 
+is.na(y) # 결측치 일 경우 TRUE
+which(is.na(y)) # TRUE가 몇번째에 위치하는지를 물어봄
+sum(is.na(y)) # TRUE는 1, FALSE는 0 : 결측치가 몇개인지 합을 내줌
