84810364c993d2e59aa6a4e4887ddf62c1bc7c11
Angela <nonlineary@naver.com>
2021-05-25 03:06:55 +0900

    Chapter 2 : Input and Output in R : Importing Data

--- Statistics-and-Data-Science/1-1-Statistical-Computing-with-R/ex02-13.R
+++ Statistics-and-Data-Science/1-1-Statistical-Computing-with-R/ex02-13.R
@@ -1,2 +1,2 @@
-nadat <- read.table('ex02-05.txt',na.strings="aa") # na.strongs="aa" aa를 NA로 인식해서 불러오게 지정함
+nadat <- read.table('ex02-05.txt',na.strings="aa") # na.strongs="aa" aa를 NA로 인식해서 불러오게 지정함 
 nadat
