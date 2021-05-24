a8561df625062d34468127abdad62aa9d24612cc
Angela <nonlineary@naver.com>
2021-05-25 02:57:39 +0900

    Chapter 2 : Input and Output in R : Manually Entering Data

--- Statistics-and-Data-Science/1-1-Statistical-Computing-with-R/ex02-03.R
+++ Statistics-and-Data-Science/1-1-Statistical-Computing-with-R/ex02-03.R
@@ -1,4 +1,4 @@
 x <- c(1,2,3,4,5)
 y <- c(10,20,30,40,50)
-dat <- cbind(x,y) # 열 단위 결합 column bind
+dat <- cbind(x,y) # 열 단위 결합 column bind 
 dat
--- Statistics-and-Data-Science/1-1-Statistical-Computing-with-R/ex02-05.R
+++ Statistics-and-Data-Science/1-1-Statistical-Computing-with-R/ex02-05.R
@@ -1,4 +1,4 @@
 dat3 <- data.frame()
-dat3 = edit(dat3) # data.frame 편집기 형태로 dat3 를 입력해서 dat3에 다시 넣겠다
+dat3 = edit(dat3) # data.frame 편집기 형태로 dat3 를 입력해서 dat3에 다시 넣겠다 
 dat3
 write.table(dat3,'ex02-05.txt')
