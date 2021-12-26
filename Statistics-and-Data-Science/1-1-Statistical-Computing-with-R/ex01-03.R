76baac31a43325a9f0ca3f2413e374a1ed7b50f8
Angela <nonlineary@naver.com>
2021-12-27 05:52:09 +0900

    Chapter 1 : Introduction to R : Basic Syntax

--- Statistics-and-Data-Science/1-1-Statistical-Computing-with-R/ex01-03.R
+++ Statistics-and-Data-Science/1-1-Statistical-Computing-with-R/ex01-03.R
@@ -1,3 +1,6 @@
-a <- "use R!"
+a <- "use R!" # 문자형 입력시 "" 안에 넣아야함
 a
-mode(a) # 변수 a의 속성을 확인해 주는 R의 내장함수 
+mode(a) # mode() : 변수 a의 속성을 확인해 주는 R의 내장함수 
+# 문자형 : character
+# 숫자형 : numeric
+# 논리형 : logical
