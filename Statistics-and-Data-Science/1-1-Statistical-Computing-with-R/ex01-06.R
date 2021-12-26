9b945aeb683346869526dca1b40abe9d7276fa3f
Angela <nonlineary@naver.com>
2021-12-27 06:19:03 +0900

    Chapter 1 : Introduction to R : Basic Usage

--- Statistics-and-Data-Science/1-1-Statistical-Computing-with-R/ex01-06.R
+++ Statistics-and-Data-Science/1-1-Statistical-Computing-with-R/ex01-06.R
@@ -1,9 +1,9 @@
 x <- c(7,2,4,9,8,6,1,5,10,3)
-sort(x) #숫자를 오름차순으로 정렬
-sum(x)
-mean(x) #평균
-sum(x)/length(x) #length() 원소의 갯수 : 합을 갯수로 나누었으몰 mean(x)와 동일한 값이 나옴
-var(x) #분산
-min(x)
+sort(x) # 숫자를 오름차순으로 정렬
+sum(x) # 합
+mean(x) # 표본본평균
+sum(x)/length(x) # length() 원소의 갯수 : 합을 갯수로 나누었으므로 mean(x)와 동일한 값이 나옴
+var(x) # 표본분산
+min(x) # 최소
 which.min(x) #최솟값이 있는 위치 : 1부터 카운팅
-summary(x) #기초 통계량 : 평균, 중간값, 최대, 최소, 사분위수(quartile) 
+summary(x) #기초 통계량 : 평균, 중간값, 최대, 최소, 사분위수(quartile)
--- Statistics-and-Data-Science/1-1-Statistical-Computing-with-R/ex01-07.R
+++ Statistics-and-Data-Science/1-1-Statistical-Computing-with-R/ex01-07.R
@@ -1,6 +1,11 @@
-y <- c(1,3,5,NA,9,NA,11) #결측치가 있을 때 함수 실행결과도 NA
-mean(y)
-mean(y,na.rm=TRUE) # 또는 na.rm=T 결측치를 무시하고 계산하고 싶을 때 
-is.na(y) # 결측치 일 경우 TRUE
+y <- c(1,3,5,NA,9,NA,11) # 결측치가 있을 때 함수 실행결과도 NA
+mean(y) # 결측치가 하나만 있어도 연산하지 못함
+mean(y,na.rm=TRUE) # 또는 na.rm=T : 결측치를 무시하고 계산하고 싶을 때 
+is.na(y) # 결측치 일 경우 TRUE : 원소 중 누가 결측치인지 알려줌
 which(is.na(y)) # TRUE가 몇번째에 위치하는지를 물어봄
-sum(is.na(y)) # TRUE는 1, FALSE는 0 : 결측치가 몇개인지 합을 내줌 
+sum(is.na(y)) # TRUE는 1, FALSE는 0 : 결측치가 몇개인지 합을 내줌 (TRUE가 몇개인지 알 수 있음) 
+
+1/0 # Inf : Infinity : 무한대
+log(-1) # NaN : Not a Number : 정의된 값이 아님 : 이상치
+# NA : Not Available : 결측치 (값이 존재하지 않음) : 손실데이터
+# NA가 포함된 상태에서 통계함수를 그냥 사용하면 연산하지 못하고 NA라는 값이 산출된다.
