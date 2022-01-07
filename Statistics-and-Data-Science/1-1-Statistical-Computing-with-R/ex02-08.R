c816d6debbf17010c3f948752ed5f8bb30329f60
Angela <nonlineary@naver.com>
2022-01-08 04:46:23 +0900

    Chapter 2 : Input and Output in R : Exporting Data

--- Statistics-and-Data-Science/1-1-Statistical-Computing-with-R/ex02-08.R
+++ Statistics-and-Data-Science/1-1-Statistical-Computing-with-R/ex02-08.R
@@ -1,6 +1,10 @@
+# write.table() : write.csv()와 비슷하지만 콤마없이 탭(공백)으로 구분
+
+
 x <- c(1,2,3,4,5)
 y <- c(10,20,30,40,50)
 dat <- cbind(x,y) # 열 단위 결합 column bind
 dat
-write.table(dat,'ex02-08.txt',sep=",") 
-# 탭으로 구분 / sep=',' 콤마로 구분 설정 
+write.table(dat,'ex02-08.txt',sep=",") # (누구를,어디에,옵션(필요할때))
+# 탭으로 구분
+# sep=',' 콤마로 구분 설정 
