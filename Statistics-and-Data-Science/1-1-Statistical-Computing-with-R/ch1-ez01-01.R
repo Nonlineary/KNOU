113b0175442911921eee6ed46c2cedd384e2a7a3
Angela <nonlineary@naver.com>
2022-01-12 10:31:07 +0900

    Chapter 1 : Introduction to R : Coursebook

---
+++ Statistics-and-Data-Science/1-1-Statistical-Computing-with-R/ch1-ez01-01.R
@@ -0,0 +1,17 @@
+# 연구과제
+
+## 01 : R의 base 패키지 중 하나인 datasets에는 많은 기본 데이터가 제공된다. 그중에서 data(faithful) 데이터를 이용하여 다음을 수행하라.
+
+### 1) 도움말 (help faithful)을 이용하여 데이터의 내용, 변수명, 소스 등을 확인하라
+help(faithful)
+
+### 2) dim() 함수를 이용하여 데이터세트의 크기를 확인하라
+dim(faithful)
+
+### 3) summary() 함수를 이용하여 변수에 대한 기술통계량을 확인하라.
+summary(faithful)
+
+### 4) plot() 명령문을 이용하여 faithful 데이터 내 두 변수에 대한 상관관계를 기술하라.
+plot(faithful) # plot() : 그래프
+
+## 02~05는 생략하여 실습해보지 않음
